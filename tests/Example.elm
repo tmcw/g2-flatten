c383c1837bd125fbc9565608734e44b78174472e
Tom MacWright <tom@macwright.org>
2019-10-10 20:52:53 -0700

    More tests

--- tests/Example.elm
+++ tests/Example.elm
@@ -19,5 +19,47 @@ suite =
                     Expect.equal
                         (flattenGeometry (Just (MultiPoint [ ( 1, 2, 0 ), ( 3, 4, 0 ) ])))
                         [ Just (Point ( 1, 2, 0 )), Just (Point ( 3, 4, 0 )) ]
+            , test "Flattens a MultiLineString into multiple line strings" <|
+                \_ ->
+                    Expect.equal
+                        (flattenGeometry
+                            (Just
+                                (MultiLineString
+                                    [ [ ( 1, 2, 0 ), ( 3, 4, 0 ) ]
+                                    , [ ( 2, 2, 0 ), ( 3, 4, 0 ) ]
+                                    ]
+                                )
+                            )
+                        )
+                        [ Just
+                            (LineString
+                                [ ( 1, 2, 0 ), ( 3, 4, 0 ) ]
+                            )
+                        , Just
+                            (LineString
+                                [ ( 2, 2, 0 ), ( 3, 4, 0 ) ]
+                            )
+                        ]
+            , test "Flattens a MultiPolygon into multiple polygons" <|
+                \_ ->
+                    Expect.equal
+                        (flattenGeometry
+                            (Just
+                                (MultiPolygon
+                                    [ [ [ ( 1, 2, 0 ), ( 3, 4, 0 ) ] ]
+                                    , [ [ ( 2, 2, 0 ), ( 3, 4, 0 ) ] ]
+                                    ]
+                                )
+                            )
+                        )
+                        [ Just
+                            (Polygon
+                                [ [ ( 1, 2, 0 ), ( 3, 4, 0 ) ] ]
+                            )
+                        , Just
+                            (Polygon
+                                [ [ ( 2, 2, 0 ), ( 3, 4, 0 ) ] ]
+                            )
+                        ]
             ]
         ]
